ad2e5a1743351e3198dc588b80faa42a4048969a
Zaid-maker <53424436+Zaid-maker@users.noreply.github.com>
2025-02-04 16:47:36 +0500

    feat: update global styles and configuration for improved theming and component integration

---
+++ components/TestSetup.tsx
@@ -0,0 +1,13 @@
+
+'use client'
+
+import React from "react"
+import { Button } from "@/components/ui/button"
+
+export default function TestSetup() {
+  return (
+    <div>
+      <Button>Click me</Button>
+    </div>
+  )
+}
